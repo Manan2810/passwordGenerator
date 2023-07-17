44299edf4c3490aa6345df188102f2ab1d2ad8a5
Manan Mehra <mananmehra330@gmail.com>
2023-07-17 18:33:31 +0530

    all the final changes are made

--- script.js
+++ script.js
@@ -26,7 +26,9 @@ function handleSlider()
 {
     inputSlider.value=passwordLength;
     lengthDisplay.innerText=passwordLength;
-
+    const min=inputSlider.min;
+    const max=inputSlider.max;
+    inputSlider.style.backgroundSize=((passwordLength-min)*100/(max-min))+"% 100%"
 }
 
 function setIndicator(color){
